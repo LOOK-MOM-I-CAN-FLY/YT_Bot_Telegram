2aec8c426d473f54783707582c090467b27de810
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-01-05 21:02:19 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,8 +1,19 @@
 package config
 
 import (
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
+
+func LoadEnv() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Файл .env не найден, используются переменные окружения из системы")
+	}
+	return err
+}
 
 func GetBotToken() string {
 	return os.Getenv("TELEGRAM_BOT_TOKEN")
