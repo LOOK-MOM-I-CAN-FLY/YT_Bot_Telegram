e86657cb0e73542e03589dd5e2654d24395b0b84
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-01-16 15:11:53 +0300

    Update handler.go

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"log"
 	"project/internal/downloader"
 	"project/internal/storage"
@@ -8,37 +9,33 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func HandleCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
-	switch message.Command() {
-	case "start":
-		msg := tgbotapi.NewMessage(message.Chat.ID, "Hi! Send me the YouTube link..")
-		bot.Send(msg)
-	default:
-		msg := tgbotapi.NewMessage(message.Chat.ID, "Unknown command.")
-		bot.Send(msg)
-	}
-}
-
 func HandleYouTubeLink(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	url := message.Text
 	chatID := message.Chat.ID
 
-	bot.Send(tgbotapi.NewMessage(chatID, "I'm downloading a video..."))
+	bot.Send(tgbotapi.NewMessage(chatID, "Downloading video..."))
 
 	filePath, err := downloader.DownloadYouTubeVideo(url)
 	if err != nil {
-		bot.Send(tgbotapi.NewMessage(chatID, "Error when uploading video."))
-		log.Println(err)
+		errorMsg := fmt.Sprintf("Failed to download video: %v", err)
+		bot.Send(tgbotapi.NewMessage(chatID, errorMsg))
+		log.Println(errorMsg)
 		return
 	}
+
+	log.Printf("Video downloaded successfully: %s", filePath)
 
 	video := tgbotapi.NewVideo(chatID, tgbotapi.FilePath(filePath))
 	_, err = bot.Send(video)
 	if err != nil {
-		log.Println("Error when sending video:", err)
+		log.Printf("Failed to send video: %v", err)
+		bot.Send(tgbotapi.NewMessage(chatID, "Error sending video."))
+		return
 	}
 
+	bot.Send(tgbotapi.NewMessage(chatID, "Video sent successfully!"))
+
 	if err := storage.CleanupDownloads(); err != nil {
-		log.Println("Error clearing downloads:", err)
+		log.Printf("Failed to clean up downloads: %v", err)
 	}
 }
