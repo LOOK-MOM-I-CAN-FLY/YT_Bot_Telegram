3f48cf8fff65a2d58488c2f5440c2d5f8babd2c1
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-01-21 23:27:54 +0300

    Update youtube.go

--- internal/downloader/youtube.go
+++ internal/downloader/youtube.go
@@ -7,28 +7,28 @@ import (
 	"path/filepath"
 )
 
-// DownloadYouTubeVideo загружает видео с YouTube по ссылке
+
 func DownloadYouTubeVideo(url string) (string, error) {
 	outputDir := "downloads"
 	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
 		os.Mkdir(outputDir, os.ModePerm)
 	}
 
-	// Генерируем путь для сохранения видео
+
 	outputTemplate := filepath.Join(outputDir, "%(title)s.%(ext)s")
 	cmd := exec.Command("yt-dlp", "-o", outputTemplate, url)
 
-	// Запускаем команду yt-dlp
+
 	if output, err := cmd.CombinedOutput(); err != nil {
 		return "", fmt.Errorf("error running yt-dlp: %v, output: %s", err, string(output))
 	}
 
-	// Находим загруженный файл
+
 	files, err := filepath.Glob(filepath.Join(outputDir, "*"))
 	if err != nil || len(files) == 0 {
 		return "", fmt.Errorf("no files found in %s: %v", outputDir, err)
 	}
 
-	// Возвращаем путь к первому файлу
+
 	return files[0], nil
 }
